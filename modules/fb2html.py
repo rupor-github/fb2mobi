7037849bd8d459d349fc570152f1c940f1d956ef
rupor-github <rupor0mail@gmail.com>
2017-01-22 05:46:06 -0500

    Special case of russian language

--- modules/fb2html.py
+++ modules/fb2html.py
@@ -479,6 +479,8 @@ class Fb2XHTML:
                             self.book_lang = t.text if len(t.text) > 2 else t.text.lower()
                         else:
                             self.book_lang = 'ru'
+                        if self.book_lang in ('rus'):
+                            self.book_lang = 'ru'
                         if self.hyphenate and self.hyphenator:
                             self.hyphenator.set_language(self.book_lang)
 
