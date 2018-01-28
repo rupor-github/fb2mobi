36ef585b78a86377d6438a425c8a1ed7848c9d3d
Oleg Serdyukov <os@fastmail.com>
2018-01-28 15:21:10 +0100

    Fix smtplib.SMTPServerDisconnected error
    While trying to send book to Fastmail and Gmail I am receiving error
    "smtplib.SMTPServerDisconnected: Connection unexpectedly closed"
    
    Tested this fix for both of these mail providers on Ubuntu 16.04 LTS.

--- modules/sendtokindle.py
+++ modules/sendtokindle.py
@@ -30,8 +30,7 @@ class SendToKindle:
             fname = os.path.basename(file_path)
             msg.attach(MIMEApplication(open(file_path, 'rb').read(), Content_Disposition='attachment; filename="%s"' % fname, Name=fname))
 
-        mail_server = smtplib.SMTP(host=self.smtp_server, port=self.smtp_port)
-        mail_server.starttls()
+        mail_server = smtplib.SMTP_SSL(host=self.smtp_server, port=self.smtp_port)
         mail_server.login(self.smtp_login, self.smtp_password)
         mail_server.sendmail(self.user_email, self.kindle_email, msg.as_string())
         mail_server.quit()
